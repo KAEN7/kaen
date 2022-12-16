d89f00de713b8e90a4ae5ee23a5c82a75b6e7839
kaen <kusdsuna@naver.com>
2022-12-17 01:02:50 +0900

    UI 초안 마무리

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,15 +8,145 @@ import { color, overflowY } from "../styles/theme";
 import Resume from "../components/Resume";
 import Image from "next/image";
 
-const list = [
+interface IResumeList {
+	company: string;
+	date: string;
+	position: string;
+	achievement: Array<string>;
+	resume: Array<{
+		title: string;
+		desc: Array<{ subTitle: string; subDesc: Array<string> }>;
+	}>;
+}
+
+const resumeList: Array<IResumeList> = [
 	{
-		company: "",
-		date: "",
-		position: "",
-		Achievement: ["", "", ""],
-		resume: [{ title: "", desc: [{ subTitle: "", subDesc: "" }] }],
+		company: "핀스프로토콜",
+		date: "2022.03 ~ 2022.11",
+		position: "개발팀/프론트엔드",
+		achievement: [],
+		resume: [
+			{
+				title: "peri.Staking",
+				desc: [
+					{
+						subTitle: "2가지의 새로운 기능 추가로 유저 유입율 향상",
+						subDesc: [
+							"Liquidation : 청산 대상자 리스트 및 모달 UI/UX 제작, 구현",
+							"Escrow : Escrow 대상 리스트 및 트랜잭션 요청",
+						],
+					},
+					{
+						subTitle:
+							"기존 로딩 속도 개선을 위해 병렬적 비동기 통신을 통해 기존 16초에서 38% 감소시켜 로딩 속도를 6초 빠르게 개선",
+						subDesc: [],
+					},
+					{
+						subTitle: "트랜잭션 요청 로직 모듈화로 복잡도를 개선",
+						subDesc: [],
+					},
+				],
+			},
+			{
+				title: "peri.Dex && peri.Dashboard",
+				desc: [
+					{
+						subTitle:
+							"기존 The Graph 통신 환경에서 GraphQL = 80%, API = 20%로 개선",
+						subDesc: [],
+					},
+					{
+						subTitle:
+							"코인 차트 및 UI/UX 개선으로 GA 기준 일평균 유입수 24명에서 38명으로 이용률을 개선",
+						subDesc: [],
+					},
+				],
+			},
+			{
+				title: "유지보수용 툴 제작",
+				desc: [
+					{
+						subTitle: "TVL_Holders",
+						subDesc: [
+							"마케팅팀에서 활용되는 지표 수집 자동화 툴 제작",
+							"PM2 cron과 Googleapis를 통해 매일 구글스프레드시트에 집계량 등록",
+						],
+					},
+					{
+						subTitle: "Monitoring Bot",
+						subDesc: [
+							"개발팀에서 사용되는 봇 에러 및 로그 메시지를 Google Chat에 전송하는 툴 제작",
+						],
+					},
+				],
+			},
+			{
+				title: "nendNFT",
+				desc: [
+					{
+						subTitle: "CloudFront를 통한 정적 웹사이트 라이브 배포",
+						subDesc: [],
+					},
+					{
+						subTitle: "퍼블리싱 및 메타마스크 dApp 브라우저 모바일 반응형 구현",
+						subDesc: [],
+					},
+				],
+			},
+		],
+	},
+	{
+		company: "예스나우",
+		date: "2021.12 ~ 2022.02",
+		position: "개발팀/프론트엔드",
+		achievement: [],
+		resume: [
+			{
+				title: "",
+				desc: [
+					{
+						subTitle:
+							"카페24 고객사 쇼핑몰에 핏파인더 설치 및 유지 보수, V2 업데이트 업무, 사이즈 이미지표 설계",
+						subDesc: [],
+					},
+					{
+						subTitle:
+							"Zeplin과 Styled-components을 통한 디자이너와 의사소통 및 협업",
+						subDesc: [],
+					},
+					{
+						subTitle:
+							"Atomic 디자인 패턴과 Storybook을 통한 컴포넌트 중심 설계 개발",
+						subDesc: [],
+					},
+					{
+						subTitle: "Admin 페이지 프론트 업무, 고객사 계정 생성",
+						subDesc: [],
+					},
+				],
+			},
+		],
 	},
 ];
+
+const skill = [
+	"aws",
+	"confluence",
+	"figma",
+	"git",
+	"javascript",
+	"jira",
+	"nextjs",
+	"notion",
+	"react",
+	"recoil",
+	"redux",
+	"styledComponents",
+	"typescript",
+	"vercel",
+];
+
+const award = ["2022.11 한화생명 드림플러스 블록체인 해커톤, 크로스트상 수상"];
 
 const Home: NextPage = () => {
 	const [_, setLoading] = useRecoilState(loadingAtom);
@@ -35,19 +165,21 @@ const Home: NextPage = () => {
 				<a href="https://kusdsuna.tistory.com/">Blog</a>
 			</HrefBox>
 
+			{/* 
 			<Image
 				src="/images/arrow.svg"
 				alt="arrow"
 				width={500}
 				height={500}
 				// placeholder="blur"
-			/>
+			/> */}
 
 			<IntroductionBox>
 				<li>리액트와 최적화에 관심이 많은 웹 프론트엔드 개발자입니다</li>
 				<li>
-					{`프론트엔드는 유저와 맨 앞에서 마주하는 직업이기에 \n 사용자 친화적으로
-					만들어야한다고 생각합니다`}
+					{
+						"프론트엔드는 유저와 맨 앞에서 마주하는 직업이기에 \n 사용자 친화적으로 만들어야한다고 생각합니다"
+					}
 				</li>
 				<li>
 					리팩토링과 최적화에 관심이 많아 개선하기 위해 많은 투자를 하고
@@ -60,7 +192,28 @@ const Home: NextPage = () => {
 			</IntroductionBox>
 
 			<SubTitle>경력</SubTitle>
-			<Resume></Resume>
+			<Resume list={resumeList}></Resume>
+
+			<SubTitle>기술</SubTitle>
+			<SkillList>
+				{skill.map((stack) => (
+					<li key={stack}>
+						<Image
+							src={`/images/skill/${stack}.png`}
+							width={85}
+							height={85}
+							alt={stack}
+						></Image>
+					</li>
+				))}
+			</SkillList>
+
+			<SubTitle>수상</SubTitle>
+			<ul>
+				{award.map((item) => (
+					<li key={item}>{item}</li>
+				))}
+			</ul>
 		</HomeSection>
 	);
 };
@@ -137,13 +290,13 @@ const IntroductionBox = styled.ul`
 	padding: 9rem 1rem;
 
 	li {
-		width: 35%;
+		width: 40rem;
 		margin-bottom: 1.5rem;
 		list-style: none;
 		font-size: 1.1rem;
 		font-weight: bold;
 		text-align: left;
-		white-space: nowrap;
+		white-space: pre-line;
 		color: ${color.white};
 		transition: all 0.3s ease 0s;
 
@@ -159,8 +312,21 @@ const IntroductionBox = styled.ul`
 `;
 
 const SubTitle = styled.h3`
-	font-size: 1.5rem;
+	width: 57rem;
+	font-size: 3rem;
 	margin-top: 3rem;
+	text-shadow: 0px 15px 20px rgba(46, 229, 157, 0.4);
+`;
+
+const SkillList = styled.ul`
+	display: flex;
+	flex-wrap: wrap;
+	width: 57rem;
+
+	li {
+		list-style: none;
+		margin: 1rem;
+	}
 `;
 
 export default Home;
